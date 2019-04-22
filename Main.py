836ab7363b597a52afdecf53e15acf8ab3a607e0
LucasPAlvarez <48615149+LucasPAlvarez@users.noreply.github.com>
2019-04-22 14:25:49 -0300

    Added Saving
    agregre la posibilidad de guardar y cargar ademas de almacenar la historia

--- Main.py
+++ Main.py
@@ -1,14 +1,38 @@
 import PoblacionIni
-#el main crea una poblacion
-myPobl = PoblacionIni.PoblacionIni()
-print (myPobl.printAll())
+import os
+import SaveandLoad as SL
+#el main crea una poblacion y es el menu de la misma
 
+
+
+# carga la poblacion anterior si existe una, si no crea una nueva al azar
+if os.path.isfile("savedPob"):
+	myPobl = SL.Loading()
+	newOne = False
+
+else:
+	myPobl = PoblacionIni.PoblacionIni()
+	newOne = True
+	
+imprimir = myPobl.printAll()
+print (imprimir)
+if newOne:
+	print("entro")
+	SL.saveRecord(imprimir)
+
+
+#el Menu empieza aqui
 while True: 
-	c=input("¿Desea crear otra generación? (S/N)" )
+	c=input("¿Desea crear otra generación? (S/N)\n" )
 	if c.lower() == "s":
-		c=input("ingrese la cantidad de generaciones")
+		c=input("ingrese la cantidad de generaciones:\n")
 		for i in range(int(c)):
 			myPobl.makeChildren()
-			print(myPobl.printAll())
+			imprimir = myPobl.printAll()
+			print(imprimir)
+			SL.saveRecord(imprimir)
 	else:
+		SL.Saving(myPobl)
 		break
+
+
